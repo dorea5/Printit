d32f4dda18b45b2d29200ca903d2b76b38ad8414
dorea <dodomokhtari@hotmail.fr>
2024-06-17 13:37:58 +0200

    incrementation decrementation version 2

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,86 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
 
-let index=0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let index = 0
+
+
+
 let ClickFlecheGauche = document.querySelector(".arrow_left");
-ClickFlecheGauche.addEventListener("click" , () => {
+ClickFlecheGauche.addEventListener("click", () => {
 
 	index--;
-	console.log(index);
-	if (index<0) {
-		index=slides.length-1;}
-		img.src=`./assets/images/slideshow/.image`;
-    console.log("eventlistener fleche gauche ok ")});
+ console.log(index);
+
+	if (index < 0) {
+		index = slides.length - 1;
+	}
+
+	
+	
+	
+	img.src = `./assets/images/slideshow/.image`;
+	text.textContent = `./assets/images/slideshow/.tagLine`;
+	console.log("eventlistener fleche gauche ok ")
+});
+
+
+
+
 
 let ClickFlecheDroite = document.querySelector(".arrow_right");
 ClickFlecheDroite.addEventListener("click", () => {
-	
+
 	index++;
 	console.log(index);
-  if (index>0) {
-		index=slides.length+1;}
-	
-img.src = `./assets/images/slideshow/.image`;
-console.log("eventlistener fleche droite ok")});
+
+
+	if (index > slides.lenght - 1) {
+		index = 0
+	}
+
+
+
+	img.src = `./assets/images/slideshow/.image`;
+	text.textContent = `./assets/images/slideshow/.tagLine`;
+	console.log("eventlistener fleche droite ok")
+});
 
 
 
@@ -49,43 +91,42 @@ console.log("eventlistener fleche droite ok")});
 
 console.log(slides);
 let SlidesNumber = slides.length;
-for (i=0; i < SlidesNumber; i ++) {	
+for (i = 0; i < SlidesNumber; i++) {
 	console.log(`tableau : ${slides[i]}`);
 	console.log(`valeur de i : ${i}`);
 	console.log(`propriété image ${slides[i].image}`);
 	// équivalent de console.log('propriété image '+slides[i].image);
 	console.log(`propriété tagline ${slides[i].tagLine}`);
-	
-	
-	}
+
+
+}
 
 
 
-	//variables et const//
-  const img = document.querySelector(".banner-img");
-  const textone = document.querySelector(" #banner p ");
-  const texttwo = document.querySelector(" #banner span ");
-	let dots = document.querySelector(".dots");
-	console.log(dots);
-	
+//variables et const//
+const img = document.querySelector("#banner img");
+const text = document.querySelector(" #banner p ");
+let dots = document.querySelector(".dots");
+console.log(dots);
+
 
 
 
 function DisplayDots() {
-	for (let i = 0 ; i < slides.length; i++){
+	for (let i = 0; i < slides.length; i++) {
 		const dot = document.createElement("div");
 		dot.classList.add("dot");
-    dots.appendChild(dot);
+		dots.appendChild(dot);
 		//etape suivante//
-//si on se trouve sur index alors classe= dots_elector//
-//boucle for: if i==? dot.classList.add("dot_selector")//
-//ce qui peut donner: if i==*page actuelle* ; dot.classList.add("dot_selector")//
+		//si on se trouve sur index alors classe= dots_elector//
+		//boucle for: if i==? dot.classList.add("dot_selector")//
+		//ce qui peut donner: if i==*page actuelle* ; dot.classList.add("dot_selector")//
 
-		if (i==index){
+		if (i == index) {
 			dot.classList.add("dot_selected")
 		}
 
-}
+	}
 
 }
 
